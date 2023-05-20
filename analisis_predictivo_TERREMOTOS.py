6a360048adce0d8afc1d02ce738094414566bc07
YossecSuarez <1313178@senati.pe>
2023-05-19 20:14:10 -0500

    ingreso de datos de los terremotos

--- analisis_predictivo_TERREMOTOS.py
+++ analisis_predictivo_TERREMOTOS.py
@@ -1,3 +1,6 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Datos históricos
+magnitudes = np.array([5.6, 6.1, 4.9, 5.2, 6.5, 7.8, 5.7, 6.3, 4.9, 8.1, 6.5])  # Magnitudes de los terremotos
+ubicaciones = np.array(['A', 'B', 'C', 'A', 'B', 'C', 'C', 'A', 'A', 'B', 'C'])  # Ubicaciones geográficas
